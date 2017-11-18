ecfbbfc90ef070713ff0c3e856d618e7776965ea
Marc-Andre <224@live.ca>
2017-11-18 09:57:15 -0500

    Better RoadMap

--- app/src/main/java/com/m224/infectious/AIComputeTask.java
+++
@@ -1,8 +0,0 @@
-package com.m224.infectious;
-
-/**
- * Created by 224 on 2017-11-18.
- */
-
-public class AIComputeTask {
-}
---
+++ app/src/main/java/com/m224/infectious/task/AIComputeTask.java
@@ -0,0 +1,14 @@
+package com.m224.infectious.task;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by 224 on 2017-11-18.
+ */
+
+public class AIComputeTask extends AsyncTask<Void, Void, Void> {
+
+
+
+    
+}
