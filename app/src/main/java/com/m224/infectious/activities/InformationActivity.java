bea37f201709da773df8ca8e2c5ffbd3b1121a98
Marc-Andre <224@live.ca>
2017-11-19 12:27:36 -0500

    Add setting fragment

--- app/src/main/java/com/m224/infectious/activities/InformationActivity.java
+++ app/src/main/java/com/m224/infectious/activities/InformationActivity.java
@@ -1,10 +1,13 @@
 package com.m224.infectious.activities;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.m224.infectious.R;
+import com.m224.infectious.fragment.SettingFragment;
 import com.m224.infectious.utils.Util;
 
 public class InformationActivity extends AppCompatActivity {
@@ -14,6 +17,15 @@ public class InformationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
         Util.customActionbar(this, R.layout.actionbar_information);
+
+        // Create new fragment and transaction
+        Fragment newFragment = new SettingFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+
+        // Commit the transaction
+        transaction.commit();
+
     }
 
     public void closeInformationActivity(View v) {
