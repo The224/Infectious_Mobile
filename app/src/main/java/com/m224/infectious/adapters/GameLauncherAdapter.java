0d73f77a68c618cf12b650f620fbfc1dd16dbfec
Marc-Andre <224@live.ca>
2017-11-20 14:05:14 -0500

    Optimize CardView

--- app/src/main/java/com/m224/infectious/adapters/GameLauncherAdapter.java
+++ app/src/main/java/com/m224/infectious/adapters/GameLauncherAdapter.java
@@ -5,12 +5,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.m224.infectious.R;
 import com.m224.infectious.activities.MainActivity;
 import com.m224.infectious.domaine.Board;
+import com.m224.infectious.utils.GridConfig;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class GameLauncherAdapter extends RecyclerView.Adapter<GameLauncherAdapter.Card> {
@@ -18,14 +21,16 @@ public class GameLauncherAdapter extends RecyclerView.Adapter<GameLauncherAdapte
     private final List<Board> boards;
     private Context context;
 
-    public class Card extends RecyclerView.ViewHolder {
+    protected class Card extends RecyclerView.ViewHolder {
         public TextView title;
+        public ImageView image;
 
         // TODO : Mettre des icons et meilleur card
 
-        public Card(View view) {
+        private Card(View view) {
             super(view);
             title = view.findViewById(R.id.title);
+            image = view.findViewById(R.id.image);
         }
     }
 
@@ -47,7 +52,18 @@ public class GameLauncherAdapter extends RecyclerView.Adapter<GameLauncherAdapte
 
         holder.title.setText(boards.get(position).getTitle());
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        if (Arrays.equals(boards.get(position).getBlockTile(), GridConfig.SQUARE))
+            holder.image.setImageResource(R.drawable.config_2);
+        else if (Arrays.equals(boards.get(position).getBlockTile(), GridConfig.BLOCK))
+            holder.image.setImageResource(R.drawable.config_3);
+        else if (Arrays.equals(boards.get(position).getBlockTile(), GridConfig.CROSS))
+            holder.image.setImageResource(R.drawable.config_4);
+        else
+            holder.image.setImageResource(R.drawable.config_1);
+
+
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mainActivity.startGameActivity(boards.get(position));
